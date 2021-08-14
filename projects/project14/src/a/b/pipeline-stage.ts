c11d4bedb394b99054c13f28663447af7e883153
Juste Méthode <justkeybocovo@gmail.com>
2021-08-15 01:04:03 +0200

    add test project (#48)

---
+++ projects/project14/src/a/b/pipeline-stage.ts
@@ -0,0 +1,5 @@
+import { PipelineBase } from '@/pipeline-base';
+
+export class PipelineStage extends PipelineBase {
+  static run() {}
+}
---
+++ projects/project14/src/pipeline.ts
@@ -0,0 +1,7 @@
+import { PipelineStage } from '@/a/b/pipeline-stage';
+
+export class Pipeline {
+  static run() {
+    PipelineStage.run();
+  }
+}
